20cabeedda27e44c0a001893a4db395d2007b4a0
John Robert Wilson <johnrobert@gmail.com>
2011-06-18 16:02:23 -0400

    Adding js callback skeleton, helper functions to module

---
+++ nodejs_buddylist/nodejs_buddylist.js
@@ -0,0 +1,9 @@
+(function ($) {
+
+Drupal.Nodejs.callbacks.nodejsBuddylist = {
+  callback: function (uid) {
+    //jquery that refreshes the selector for buddy list, or user list
+  }
+};
+
+})(jQuery);
