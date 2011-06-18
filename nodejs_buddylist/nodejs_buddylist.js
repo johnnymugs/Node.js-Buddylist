072f165eea6a448bde63a8954d6d61f560f2caa4
John Robert Wilson <johnrobert@gmail.com>
2011-06-18 18:59:17 -0400

    Added nodejs_buddylist_overseer_page function and updated the js file

--- nodejs_buddylist/nodejs_buddylist.js
+++ nodejs_buddylist/nodejs_buddylist.js
@@ -1,7 +1,7 @@
 (function ($) {
 
-Drupal.Nodejs.callbacks.nodejsBuddylist = {
-  callback: function (uid) {
+Drupal.Nodejs.callbacks.nodejsBuddylist.refreshList = {
+  callback: function (container_id, data) {
     //jquery that refreshes the selector for buddy list, or user list
   }
 };
